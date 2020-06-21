f99d0e4a2b7c165ad3fed877d6c29f1435f6f23d
geni <me@geni.site>
2020-06-21 18:35:08 +0300

    Update Windows patches to 0.4.26

--- src/checks.c
+++ src/checks.c
@@ -5,7 +5,7 @@
 	static const check checkname = {#checkname, _address, sizeof check_sig_##checkname / sizeof check_sig_##checkname[0], check_sig_##checkname}
 
 #ifdef _WIN32
-CHECK(version_check, 0x3AEE50, "0.4.25");
+CHECK(version_check, 0x3B36C0, "0.4.26");
 #elif __linux__
 CHECK(version_check, 0x4C2A7D, "0.4.25");
 #elif __APPLE__
--- src/patches.c
+++ src/patches.c
@@ -5,11 +5,11 @@
 	static const patch patchname = {#patchname, _address, sizeof patch_sig_##patchname / sizeof patch_sig_##patchname[0], patch_sig_##patchname}
 
 #ifdef _WIN32
-PATCH(deleted_message_space_length, 0x1709CE, {0x08});
-PATCH(deleted_message_details, 0xE37EC, {0xEB});
-PATCH(deleted_message_append, 0x1709FC, {0xEB, 0x4C});
-PATCH(deleted_message_space, 0x3D64A9, {0x20});
-PATCH(cooldown, 0x14A1E4, {0x90, 0x90, 0xB1, 0x00, 0x90});
+PATCH(deleted_message_space, 0x3DAE39, {0x20});
+PATCH(deleted_message_space_length, 0x174B9E, {0x08});
+PATCH(deleted_message_append, 0x174BCC, {0xEB, 0x4C});
+PATCH(deleted_message_details, 0xE7C5C, {0xEB});
+PATCH(cooldown, 0x14E3B4, {0x90, 0x90, 0xB1, 0x00, 0x90});
 
 const patch* patches[] = {
 	&deleted_message_space_length, &deleted_message_details,
